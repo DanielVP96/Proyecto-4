66ef22c3a6ad4557b93667f132960a246d3532f9
DanielVP96 <villegasdaniel57@gmail.com>
2025-12-15 13:28:35 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import MST as proyecto_4
+
+
+
+def main():
+    try:
+
+        #Proyecto 3 - Algoritmo de Dijkstra
+        proyecto_4.mst()
+
+    except Exception as err:
+        print(err)
+
+main()
